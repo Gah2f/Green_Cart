4457506e583c7fdd9c8ba216315b98e00755af22
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2025-04-21 15:28:24 +0300

    Tostify added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Nav from "./components/Nav";
 import { Routes, Route, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   const isSellerPath = useLocation().pathname.includes("seller");
   return (
     <div>
-      <Nav />
+      {isSellerPath ? null : <Nav /> } 
+      <Toaster />
       <div className={`${isSellerPath ? " " : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
           <Route path="/" element={<Home />} />
