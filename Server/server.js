3cddf368d2024ec9fd0324d72ff415668821d7ec
Nebiyu Daniel <danielnebiyuneb@gmail.com>
2025-04-30 10:46:55 +0300

    Server js edited

--- Server/server.js
+++ Server/server.js
@@ -15,7 +15,7 @@ import { stripeWebhooks } from "./controllers/orderController.js";
 const app = express();
 const port = process.env.PORT || 4000;
 
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["http://localhost:5173", "https://green-cart-mocha.vercel.app"];
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks);
 // Middleware
 
